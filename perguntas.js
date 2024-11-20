4b75dd9c576ab6a80695aef2ee79ce7eaa6a1315
maisa020 <00001092051260sp@al.educacao.sp.gov.br>
2024-11-20 15:01:04 -0300

    adicionando arquivos

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Escola',
+    'Com quem eu me sento na escola?',
+    'Com a Duda Bueno'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual o maior continente em extensão?',
+    'Ásia'
+)
+
+criaCartao(
+    'Geografia',
+    'Em qual continente fica a Rússia?',
+    'Eurásia (Europa e Ásia)'
+)
+
+criaCartao(
+    'Aulas',
+    'Quantas aulas temos por dia?',
+    '9'
+)
